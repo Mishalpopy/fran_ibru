8fcaa68de2b25bd23e52abb85db006af42475973
MishalPopy <c.mishal46@gmail.com>
2022-06-03 14:50:57 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def app1 (request):
+    return render(request, 'app1.html')
+
+
+def index (request):
+    return HttpResponse('abc')
