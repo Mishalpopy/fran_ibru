ff4ba326751020b37da7a618d8af58d10638dd09
MishalPopy <c.mishal46@gmail.com>
2022-06-09 14:56:57 +0530

    'lastheroku'

--- ibruproject/settings.py
+++ ibruproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,4 +127,4 @@ staticfiles_DIRS = (os.path.join(BASE_DIR,'static'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
